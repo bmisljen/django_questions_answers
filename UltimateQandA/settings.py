951291ba99fcdbc98c491c74c58421cc4d1f6d90
bmisljen <bmisljen@gmail.com>
2018-04-15 14:11:04 -0400

    Heorku updates 10

--- UltimateQandA/settings.py
+++ UltimateQandA/settings.py
@@ -139,9 +139,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'qanda/static'),
-)
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'qanda/static'),
+#)
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
